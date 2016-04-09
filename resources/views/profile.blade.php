4c34a1ea07a47cd0056d187e2148c769337d347e
Danang Adi Nugroho <danang13@mhs.if.its.ac.id>
2016-04-09 23:52:31 +0700

    view profil

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -40,6 +40,23 @@
                     </button>
                 </div>
 
+                <!-- Pencarian -->
+                <div class="collapse navbar-collapse pull-left" id="navbar-collapse">
+                  <form class="navbar-form navbar-left" role="search">
+                    <button type="button" style="height: 34px" class="btn btn-default dropdown-toggle" data-toggle="dropdown">Kategori
+                      <span class="fa fa-caret-down"></span>
+                    </button>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">Laut</a></li>
+                        <li><a href="#">Tawar</a></li>
+                    </ul>
+                    <div class="form-group">
+                      <input type="text" class="form-control" id="navbar-search-input" placeholder="Cari">
+                      <button type="button" style="height: 34px" class="btn btn-info btn-flat">Cari</button>
+                    </div>
+                  </form>
+                </div>
+
                 <!-- Login -->
                 <div class="navbar-custom-menu">
                     <ul class="nav navbar-nav">
@@ -140,29 +157,298 @@
     </header>
     <!-- Full Width Column -->
     <div class="content-wrapper">
+
         <div class="container">
             <!-- Content Header (Page header) -->
             <section class="content-header">
-                <h1>
-                    Pencarian
-                </h1>
-                <div class="input-group margin">
-                    <div class="input-group-btn">
-                        <button type="button" style="height: 50px" class="btn btn-default dropdown-toggle" data-toggle="dropdown">Kategori
-                            <span class="fa fa-caret-down"></span></button>
-                        <ul class="dropdown-menu">
-                            <li><a href="#">Laut</a></li>
-                            <li><a href="#">Tawar</a></li>
-                        </ul>
+              <h1>
+                Profil
+              </h1>
+            </section>
+
+            <!-- Main content -->
+            <section class="content">
+
+              <div class="row">
+                <div class="col-md-3">
+
+                  <!-- Profile Image -->
+                  <div class="box box-primary">
+                    <div class="box-body box-profile">
+                      <img class="profile-user-img img-responsive img-circle" src="../../dist/img/user4-128x128.jpg" alt="User profile picture">
+
+                      <h3 class="profile-username text-center">Nama Lengkap</h3>
+
+                      <p class="text-muted text-center">Username</p>
+
                     </div>
-                    <!-- /btn-group -->
-                    <input type="text" class="form-control" style="height: 50px">
-                    <span class="input-group-btn">
-                      <button type="button" style="height: 50px" class="btn btn-info btn-flat">Go!</button>
-                    </span>
+                    <!-- /.box-body -->
+                  </div>
+                  <!-- /.box -->
+
+                  <!-- About Me Box -->
+                  <div class="box box-primary">
+                    <div class="box-header with-border">
+                      <h3 class="box-title">About Me</h3>
+                    </div>
+                    <!-- /.box-header -->
+                    <div class="box-body">
+                      <strong><i class="fa fa-user margin-r-5"></i> Email</strong>
+
+                      <p class="text-muted">
+                        About
+                        Kelamin
+                        dll
+                      </p>
+
+                      <hr>
+
+                      <strong><i class="fa fa-envelope margin-r-5"></i> Email</strong>
+
+                      <p class="text-muted">
+                        E-mail
+                      </p>
+
+                      <hr>
+
+                      <strong><i class="fa fa-mobile margin-r-5"></i> Kontak</strong>
+
+                      <p class="text-muted">
+                        Telpon
+                      </p>
+
+                      <hr>
+
+                      <strong><i class="fa fa-map-marker margin-r-5"></i> Lokasi</strong>
+
+                      <p class="text-muted">
+                        Lokasi
+                      </p>
+
+                      <hr>
+
+                      <strong><i class="fa fa-star margin-r-5"></i> Rating</strong>
+
+                      <p>Rating</p>
+                    </div>
+                    <!-- /.box-body -->
+                  </div>
+                  <!-- /.box -->
                 </div>
+                <!-- /.col -->
+                <div class="col-md-9">
+                  <div class="nav-tabs-custom">
+                    <ul class="nav nav-tabs">
+                      <li class="active"><a href="#activity" data-toggle="tab">Activity</a></li>
+                      <li><a href="#timeline" data-toggle="tab">Timeline</a></li>
+                      <li><a href="#settings" data-toggle="tab">Settings</a></li>
+                      <li><a href="#moresettings" data-toggle="tab">More Settings</a></li>
+                    </ul>
+                    <div class="tab-content">
+                      <div class="active tab-pane" id="activity">
+                        <!-- Post -->
+                        <div class="post">
+                          <div class="user-block">
+                            <img class="img-circle img-bordered-sm" src="../../dist/img/user1-128x128.jpg" alt="user image">
+                                <span class="username">
+                                  <a href="#">Jonathan Burke Jr.</a>
+                                  <a href="#" class="pull-right btn-box-tool"><i class="fa fa-times"></i></a>
+                                </span>
+                            <span class="description">Shared publicly - 7:30 PM today</span>
+                          </div>
+                          <!-- /.user-block -->
+                          <p>
+                            Lorem ipsum represents a long-held tradition for designers,
+                            typographers and the like. Some people hate it and argue for
+                            its demise, but others ignore the hate as they create awesome
+                            tools to help create filler text for everyone from bacon lovers
+                            to Charlie Sheen fans.
+                          </p>
+                          <ul class="list-inline">
+                            <li><a href="#" class="link-black text-sm"><i class="fa fa-share margin-r-5"></i> Share</a></li>
+                            <li><a href="#" class="link-black text-sm"><i class="fa fa-thumbs-o-up margin-r-5"></i> Like</a>
+                            </li>
+                            <li class="pull-right">
+                              <a href="#" class="link-black text-sm"><i class="fa fa-comments-o margin-r-5"></i> Comments
+                                (5)</a></li>
+                          </ul>
+
+                          <input class="form-control input-sm" type="text" placeholder="Type a comment">
+                        </div>
+                        <!-- /.post -->
+
+                      </div>
+                      <!-- /.tab-pane -->
+                      <div class="tab-pane" id="timeline">
+                        <!-- The timeline -->
+                        <ul class="timeline timeline-inverse">
+                          <!-- timeline time label -->
+                          <li class="time-label">
+                                <span class="bg-red">
+                                  10 Feb. 2014
+                                </span>
+                          </li>
+                          <!-- /.timeline-label -->
+                          <!-- timeline item -->
+                          <li>
+                            <i class="fa fa-envelope bg-blue"></i>
+
+                            <div class="timeline-item">
+                              <span class="time"><i class="fa fa-clock-o"></i> 12:05</span>
+
+                              <h3 class="timeline-header"><a href="#">Support Team</a> sent you an email</h3>
+
+                              <div class="timeline-body">
+                                Etsy doostang zoodles disqus groupon greplin oooj voxy zoodles,
+                                weebly ning heekya handango imeem plugg dopplr jibjab, movity
+                                jajah plickers sifteo edmodo ifttt zimbra. Babblely odeo kaboodle
+                                quora plaxo ideeli hulu weebly balihoo...
+                              </div>
+                              <div class="timeline-footer">
+                                <a class="btn btn-primary btn-xs">Read more</a>
+                                <a class="btn btn-danger btn-xs">Delete</a>
+                              </div>
+                            </div>
+                          </li>
+                          <!-- END timeline item -->
+                          <!-- timeline item -->
+                          <li>
+                            <i class="fa fa-user bg-aqua"></i>
+
+                            <div class="timeline-item">
+                              <span class="time"><i class="fa fa-clock-o"></i> 5 mins ago</span>
+
+                              <h3 class="timeline-header no-border"><a href="#">Sarah Young</a> accepted your friend request
+                              </h3>
+                            </div>
+                          </li>
+                          <!-- END timeline item -->
+                          <!-- timeline item -->
+                          <li>
+                            <i class="fa fa-comments bg-yellow"></i>
+
+                            <div class="timeline-item">
+                              <span class="time"><i class="fa fa-clock-o"></i> 27 mins ago</span>
+
+                              <h3 class="timeline-header"><a href="#">Jay White</a> commented on your post</h3>
+
+                              <div class="timeline-body">
+                                Take me to your leader!
+                                Switzerland is small and neutral!
+                                We are more like Germany, ambitious and misunderstood!
+                              </div>
+                              <div class="timeline-footer">
+                                <a class="btn btn-warning btn-flat btn-xs">View comment</a>
+                              </div>
+                            </div>
+                          </li>
+                          <!-- END timeline item -->
+                          <!-- timeline time label -->
+                          <li class="time-label">
+                                <span class="bg-green">
+                                  3 Jan. 2014
+                                </span>
+                          </li>
+                          <!-- /.timeline-label -->
+                          <!-- timeline item -->
+                          <li>
+                            <i class="fa fa-camera bg-purple"></i>
+
+                            <div class="timeline-item">
+                              <span class="time"><i class="fa fa-clock-o"></i> 2 days ago</span>
+
+                              <h3 class="timeline-header"><a href="#">Mina Lee</a> uploaded new photos</h3>
+
+                              <div class="timeline-body">
+                                <img src="http://placehold.it/150x100" alt="..." class="margin">
+                                <img src="http://placehold.it/150x100" alt="..." class="margin">
+                                <img src="http://placehold.it/150x100" alt="..." class="margin">
+                                <img src="http://placehold.it/150x100" alt="..." class="margin">
+                              </div>
+                            </div>
+                          </li>
+                          <!-- END timeline item -->
+                          <li>
+                            <i class="fa fa-clock-o bg-gray"></i>
+                          </li>
+                        </ul>
+                      </div>
+                      <!-- /.tab-pane -->
+
+                      <div class="tab-pane" id="settings">
+                        <form class="form-horizontal">
+                          <div class="form-group">
+                            <label for="inputName" class="col-sm-2 control-label">Nama Lengkap</label>
+
+                            <div class="col-sm-10">
+                              <input type="email" class="form-control" id="inputName" placeholder="Nama Lengkap">
+                            </div>
+                          </div>
+                          <div class="form-group">
+                            <label for="inputName" class="col-sm-2 control-label">Kontak</label>
+
+                            <div class="col-sm-10">
+                              <input type="text" class="form-control" id="inputName" placeholder="Kontak">
+                            </div>
+                          </div>
+                          <div class="form-group">
+                            <label for="inputExperience" class="col-sm-2 control-label">About</label>
+
+                            <div class="col-sm-10">
+                              <textarea class="form-control" id="inputExperience" placeholder="About"></textarea>
+                            </div>
+                          </div>
+                          <div class="form-group">
+                            <div class="col-sm-offset-2 col-sm-10">
+                              <button type="submit" class="btn btn-danger">Submit</button>
+                            </div>
+                          </div>
+                        </form>
+                      </div>
+                      <!-- /.tab-pane -->
+
+                      <div class="tab-pane" id="moresettings">
+                        <form class="form-horizontal">
+                          <div class="form-group">
+                            <label for="inputEmail" class="col-sm-2 control-label">Email</label>
+
+                            <div class="col-sm-10">
+                              <input type="email" class="form-control" id="inputEmail" placeholder="Email">
+                            </div>
+                          </div>
+                          <div class="form-group">
+                            <label for="inputName" class="col-sm-2 control-label">Password Lama</label>
+
+                            <div class="col-sm-10">
+                              <input type="text" class="form-control" id="inputName" placeholder="Password Lama">
+                            </div>
+                          </div>
+                          <div class="form-group">
+                            <label for="inputName" class="col-sm-2 control-label">Password Baru</label>
+
+                            <div class="col-sm-10">
+                              <input type="text" class="form-control" id="inputName" placeholder="Password Baru">
+                            </div>
+                          </div>
+                          <div class="form-group">
+                            <div class="col-sm-offset-2 col-sm-10">
+                              <button type="submit" class="btn btn-danger">Submit</button>
+                            </div>
+                          </div>
+                        </form>
+                      </div>
+                      <!-- /.tab-pane -->
+                    </div>
+                    <!-- /.tab-content -->
+                  </div>
+                  <!-- /.nav-tabs-custom -->
+                </div>
+                <!-- /.col -->
+              </div>
+              <!-- /.row -->
 
             </section>
+            <!-- /.content -->
 
         </div>
         <!-- /.container -->

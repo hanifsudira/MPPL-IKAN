dea6ee79d2d0a5d2651329b0ab0a988ed66d7056
Danang Adi Nugroho <danang13@mhs.if.its.ac.id>
2016-04-10 00:17:04 +0700

    view list search

--- resources/views/listsearch.blade.php
+++ resources/views/listsearch.blade.php
@@ -164,6 +164,111 @@
 
             </section>
 
+            <section class="content">
+                <div class="box box-primary">
+                    <div class="box-header with-border">
+                        <i class="fa fa-newspaper-o"></i>
+                        <h3 class="box-title">Post</h3>
+                    </div>
+
+                    <div class="box-body">
+                        <!-- Post -->
+                        <div class="post">
+                          <div class="user-block">
+                            <img class="img-circle img-bordered-sm" src="../../dist/img/user1-128x128.jpg" alt="user image">
+                                <span class="username">
+                                  <a href="#">Jonathan Burke Jr.</a>
+                                  <a href="#" class="pull-right btn-box-tool"><i class="fa fa-times"></i></a>
+                                </span>
+                            <span class="description">Shared publicly - 7:30 PM today</span>
+                          </div>
+                          <!-- /.user-block -->
+                          <p>
+                            Lorem ipsum represents a long-held tradition for designers,
+                            typographers and the like. Some people hate it and argue for
+                            its demise, but others ignore the hate as they create awesome
+                            tools to help create filler text for everyone from bacon lovers
+                            to Charlie Sheen fans.
+                          </p>
+                          <ul class="list-inline">
+                            <li><a href="#" class="link-black text-sm"><i class="fa fa-share margin-r-5"></i> Share</a></li>
+                            <li><a href="#" class="link-black text-sm"><i class="fa fa-thumbs-o-up margin-r-5"></i> Like</a>
+                            </li>
+                            <li class="pull-right">
+                              <a href="#" class="link-black text-sm"><i class="fa fa-comments-o margin-r-5"></i> Comments
+                                (5)</a></li>
+                          </ul>
+
+                          <input class="form-control input-sm" type="text" placeholder="Type a comment">
+                        </div>
+                        <!-- /.post -->
+
+                        <!-- Post -->
+                        <div class="post">
+                          <div class="user-block">
+                            <img class="img-circle img-bordered-sm" src="../../dist/img/user1-128x128.jpg" alt="user image">
+                                <span class="username">
+                                  <a href="#">Jonathan Burke Jr.</a>
+                                  <a href="#" class="pull-right btn-box-tool"><i class="fa fa-times"></i></a>
+                                </span>
+                            <span class="description">Shared publicly - 7:30 PM today</span>
+                          </div>
+                          <!-- /.user-block -->
+                          <p>
+                            Lorem ipsum represents a long-held tradition for designers,
+                            typographers and the like. Some people hate it and argue for
+                            its demise, but others ignore the hate as they create awesome
+                            tools to help create filler text for everyone from bacon lovers
+                            to Charlie Sheen fans.
+                          </p>
+                          <ul class="list-inline">
+                            <li><a href="#" class="link-black text-sm"><i class="fa fa-share margin-r-5"></i> Share</a></li>
+                            <li><a href="#" class="link-black text-sm"><i class="fa fa-thumbs-o-up margin-r-5"></i> Like</a>
+                            </li>
+                            <li class="pull-right">
+                              <a href="#" class="link-black text-sm"><i class="fa fa-comments-o margin-r-5"></i> Comments
+                                (5)</a></li>
+                          </ul>
+
+                          <input class="form-control input-sm" type="text" placeholder="Type a comment">
+                        </div>
+                        <!-- /.post -->
+
+                        <!-- Post -->
+                        <div class="post">
+                          <div class="user-block">
+                            <img class="img-circle img-bordered-sm" src="../../dist/img/user1-128x128.jpg" alt="user image">
+                                <span class="username">
+                                  <a href="#">Jonathan Burke Jr.</a>
+                                  <a href="#" class="pull-right btn-box-tool"><i class="fa fa-times"></i></a>
+                                </span>
+                            <span class="description">Shared publicly - 7:30 PM today</span>
+                          </div>
+                          <!-- /.user-block -->
+                          <p>
+                            Lorem ipsum represents a long-held tradition for designers,
+                            typographers and the like. Some people hate it and argue for
+                            its demise, but others ignore the hate as they create awesome
+                            tools to help create filler text for everyone from bacon lovers
+                            to Charlie Sheen fans.
+                          </p>
+                          <ul class="list-inline">
+                            <li><a href="#" class="link-black text-sm"><i class="fa fa-share margin-r-5"></i> Share</a></li>
+                            <li><a href="#" class="link-black text-sm"><i class="fa fa-thumbs-o-up margin-r-5"></i> Like</a>
+                            </li>
+                            <li class="pull-right">
+                              <a href="#" class="link-black text-sm"><i class="fa fa-comments-o margin-r-5"></i> Comments
+                                (5)</a></li>
+                          </ul>
+
+                          <input class="form-control input-sm" type="text" placeholder="Type a comment">
+                        </div>
+                        <!-- /.post -->
+                    </div>
+                </div>
+            </section>
+            <!-- /.content -->
+
         </div>
         <!-- /.container -->
     </div>

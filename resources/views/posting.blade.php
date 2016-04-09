283ee3f64ddde02f6639724dbcac6a76583856dd
Danang Adi Nugroho <danang13@mhs.if.its.ac.id>
2016-04-10 00:53:44 +0700

    view post baru

--- resources/views/posting.blade.php
+++ resources/views/posting.blade.php
@@ -40,6 +40,23 @@
                     </button>
                 </div>
 
+                <!-- Pencarian -->
+                <div class="collapse navbar-collapse pull-left" id="navbar-collapse">
+                  <form class="navbar-form navbar-left" role="search">
+                    <button type="button" style="height: 34px" class="btn btn-default dropdown-toggle" data-toggle="dropdown">Kategori
+                      <span class="fa fa-caret-down"></span>
+                    </button>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">Laut</a></li>
+                        <li><a href="#">Tawar</a></li>
+                    </ul>
+                    <div class="form-group">
+                      <input type="text" class="form-control" id="navbar-search-input" placeholder="Cari">
+                      <button type="button" style="height: 34px" class="btn btn-info btn-flat">Cari</button>
+                    </div>
+                  </form>
+                </div>
+
                 <!-- Login -->
                 <div class="navbar-custom-menu">
                     <ul class="nav navbar-nav">
@@ -144,24 +161,46 @@
             <!-- Content Header (Page header) -->
             <section class="content-header">
                 <h1>
-                    Pencarian
+                    Post . . .
                 </h1>
-                <div class="input-group margin">
-                    <div class="input-group-btn">
-                        <button type="button" style="height: 50px" class="btn btn-default dropdown-toggle" data-toggle="dropdown">Kategori
-                            <span class="fa fa-caret-down"></span></button>
-                        <ul class="dropdown-menu">
-                            <li><a href="#">Laut</a></li>
-                            <li><a href="#">Tawar</a></li>
-                        </ul>
-                    </div>
-                    <!-- /btn-group -->
-                    <input type="text" class="form-control" style="height: 50px">
-                    <span class="input-group-btn">
-                      <button type="button" style="height: 50px" class="btn btn-info btn-flat">Go!</button>
-                    </span>
-                </div>
+            </section>
 
+            <!-- Main content -->
+            <section class="content">
+              <div class="box box-info">
+                <div class="box-body">
+                    <!-- Post -->
+                    <div class="post">
+                      <div class="user-block">
+                        <img class="img-circle img-bordered-sm" src="../../dist/img/user1-128x128.jpg" alt="user image">
+                            <span class="username">
+                              <a href="#">Jonathan Burke Jr.</a>
+                              <a href="#" class="pull-right btn-box-tool"><i class="fa fa-times"></i></a>
+                            </span>
+                        <span class="description">Shared publicly - 7:30 PM today</span>
+                      </div>
+                      <!-- /.user-block -->
+                      <p>
+                        Lorem ipsum represents a long-held tradition for designers,
+                        typographers and the like. Some people hate it and argue for
+                        its demise, but others ignore the hate as they create awesome
+                        tools to help create filler text for everyone from bacon lovers
+                        to Charlie Sheen fans.
+                      </p>
+                      <ul class="list-inline">
+                        <li><a href="#" class="link-black text-sm"><i class="fa fa-share margin-r-5"></i> Share</a></li>
+                        <li><a href="#" class="link-black text-sm"><i class="fa fa-thumbs-o-up margin-r-5"></i> Like</a>
+                        </li>
+                        <li class="pull-right">
+                          <a href="#" class="link-black text-sm"><i class="fa fa-comments-o margin-r-5"></i> Comments
+                            (5)</a></li>
+                      </ul>
+
+                      <input class="form-control input-sm" type="text" placeholder="Type a comment">
+                    </div>
+                    <!-- /.post -->
+                </div>
+              </div>
             </section>
 
         </div>

5305b47b23736f160da1fb4289da50285b65f2d2
ghulamfajri <ghulamfajri13@gmail.com>
2016-04-18 00:52:38 +0700

    database migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,16 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
+            $table->increments('id_user');
+            $table->string('username')->unique();
+            $table->string('fullname');
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('jenis_kelamin');
+            $table->date('tanggal_lahir');
+            $table->string('tempat_lahir');
+            $table->text('rating');
+            $table->string('path_foto');
             $table->rememberToken();
             $table->timestamps();
         });
